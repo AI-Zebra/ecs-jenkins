27ec30d3601c3237b23d1e5bcc00dd2f790b96dd
gururajs15 <gururajs_15@outlook.com>
2021-08-24 19:18:20 +0530

    Update convertjson.sh

--- convertjson.sh
+++ convertjson.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 cat ./ecs/container-definition-update-image.json | sed 's/^.\(.*\).$/\1/'  > 9.json
 /usr/bin/spruce merge 9.json 3.json | spruce json > 5.json | cat 5.json
+cat 5.json | sed  -i '1i /path/of/file.sh' > 10.json
+cat 10.json | sed -e '$a]' > 11.json
